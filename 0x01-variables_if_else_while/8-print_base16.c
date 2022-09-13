d46ec47105d7e7c76abe770582ba4772b4f545e9
Vasco21 <vascoeti123@gmail.com>
2022-09-13 01:43:44 -0700

    numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 	int d;
 	char low;
 
-	for (d = 0; d < 10'; d++)
-		putchar((d % 10) + '0');
+	for (d = '0'; d <= '9'; d++)
+		printf("%d",d);
 	for (low = 'a'; low <= 'f'; low++)
-		putchar(low);
-	putchar("\n");
+		printf("%c", low);
+	printf("\n");
 
 	return (0);
 }
