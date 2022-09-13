46805766c81ac7e732730e3e05825f91688a3862
Vasco21 <vascoeti123@gmail.com>
2022-09-13 02:31:35 -0700

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,38 +1,32 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	int a;
-	int b;
-	int c;
-
-	a = 0;
-	while(a <= '7') 
-	{ 
-		b = a + 1;
-	       	while(b <= '8')
-	       	{ 
-			c = b + 1;
-		       	while(c <= '9') 
-			{
-			       cm_putchar(a, b, c) 
-			       {
-				       if(a != '7' || b != '8' || c != '9')
-				       { 
-					       putchar(',');
-					       putchar(' ');
-				       } 
-				       c++; 
-			       } b++; 
-			}
-		       	a++; 
+	
+	while (i <= '7') 
+	{
+		j = i + 1;
+		while (j <= '8') 
+		{
+       
+		       	k = j + 1;
+        	while (k <= '9') 
+		{
+            
+			putchar(i);
+            		putchar(j);
+            		putchar(k);
+            		putchar(',');
+            		putchar(' ');
+		k++;
 		}
-       	} 
-} 
+		j++;
+    	}
+    	i++;
+}
