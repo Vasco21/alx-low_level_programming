30edc018bef50afa675678cc2bcbf0f3b0d83318
Vasco21 <vascoeti123@gmail.com>
2022-09-13 20:47:27 -0700

    done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,7 +6,9 @@
 #include "main.h"
 
 /**
-* Description: Function that adds two integers and returns the result
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Function that adds two integers and returns the result
  * Return: Result to add a and b
  */
 
