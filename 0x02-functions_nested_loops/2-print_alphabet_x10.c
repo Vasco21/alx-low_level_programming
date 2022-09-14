f78bf705af134f42412d80d4da2d06ab915eeb92
Vasco21 <vascoeti123@gmail.com>
2022-09-13 18:59:14 -0700

    count letters

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,7 +14,7 @@ void print_alphabet_x10(void)
 	int counter = 0;
 	char alpha;
 
-	while(counter++ <= 9)
+	while (counter++ <= 9)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
