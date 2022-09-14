819dea605b8fd5d48189091b4f80421406adaf8b
Vasco21 <vascoeti123@gmail.com>
2022-09-13 17:18:40 -0700

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * main - Prints "HeloWorld" followed by a new line.
+ * main - Prints "_putchar" followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 
-	_putchar('H');
-	_putchar('e');
-	_putchar('l');
-	_putchar('0');
-	_putchar('W');
-	_putchar('o');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
 	_putchar('r');
-	_putchar('l');
-	_putchar('d');
 	_putchar('\n');
 	return (0);
 }
