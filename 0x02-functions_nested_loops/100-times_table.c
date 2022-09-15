deef96ef2389ff8a84b17c5b45848cacad8f7ad2
Vasco21 <vascoeti123@gmail.com>
2022-09-14 19:10:10 -0700

    table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,17 +10,44 @@
  * @n: The multiplication table requested.
  * Return: Nothing.
  */
-
 void print_times_table(int n)
 {
-	int j, i, n;
+	int i, j, res;
 
-	for (i = 1; i < 10; i++)
+	if (!(n > 15 || n < 0))
 	{
-		for (j = 1; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			printf("%dx%d = %d, ",j,i,i*j)
+			for (j = 0; j <= n; j++)
+			{
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
 		}
-		printf("\n")
 	}
 }
