ecc9b2e495ee261d843610ae08ddfde47d9c325d
Vasco21 <vascoeti123@gmail.com>
2022-09-21 21:34:01 -0700

    rot

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,4 @@
-/*
+i/*
  * File:rot23.c
  *Auth: Vasco Eti
  */
@@ -12,21 +12,22 @@
  */
 char *rot13(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	int i, j;
+	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char boolean;
+	
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
+		boolean = 0;
+		for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
 		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
+			if (s[i] == alpha[j])
+			{
+				s[i] = r[j];
+				boolean = 1;
+			}
 		}
-		i++;
 	}
 	return (s);
 }
