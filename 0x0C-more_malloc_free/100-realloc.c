8c604a633f2f0425227d222466cb0690e8d25bb0
Vasco21 <vascoeti123@gmail.com>
2022-10-06 19:44:04 -0700

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,77 +3,41 @@
  */
 
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
- * _realloc - A function that reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated
- * @old_size: old size in bytes of memory llocated space
- * @new_size: new size in bytes of memory to be allocated
- * Return: void pointer to new memory location
+ * *_realloc - reallocate memory size function
+ * @ptr: pointer to address of old memory location
+ * @old_size: unsigned int type of old memory size
+ * @new_size: unsigned int type for new memory size
+ * Return:  return pointer to array
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_location, *new_ptr;
-	unsigned int i = 0;
+	char *s;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL) /* treat as normal malloc */
+	if (new_size > old_size)
 	{
-		ptr = malloc(new_size);
-		if (ptr == NULL)
-			return (NULL);
+		s = malloc(new_size);
+		free(ptr);
+		return (s);
+	}
+	if (new_size == old_size)
+	{
 		return (ptr);
 	}
-	if (new_size == 0 && ptr)
+	if (ptr == NULL)
+	{
+		s = malloc(new_size);
+		free(ptr);
+		return (s);
+	}
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	new_location = malloc(new_size);
-	new_ptr = ptr;
-	if (old_size > new_size)
-		old_size = new_size;
-	for (i = 0; i < old_size; i++)
-		new_location[i] = new_ptr[i];
-	free(ptr);
-	return (new_location);
-}
-#include <stdlib.h>
-
-/**
- * _realloc - A function that reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated
- * @old_size: old size in bytes of memory llocated space
- * @new_size: new size in bytes of memory to be allocated
- * Return: void pointer to new memory location
- */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	char *new_location, *new_ptr;
-	unsigned int i = 0;
-
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL) /* treat as normal malloc */
-	{
-		ptr = malloc(new_size);
-		if (ptr == NULL)
-			return (NULL);
-		return (ptr);
-	}
-	if (new_size == 0 && ptr)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	new_location = malloc(new_size);
-	new_ptr = ptr;
-	if (old_size > new_size)
-		old_size = new_size;
-	for (i = 0; i < old_size; i++)
-		new_location[i] = new_ptr[i];
-	free(ptr);
-	return (new_location);
+	return (ptr);
 }
