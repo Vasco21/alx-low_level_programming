7df465e1882dd3fe9e2e58339349758e226ca137
Vasco21 <vascoeti123@gmail.com>
2022-10-06 20:30:12 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,3 @@
-/*
- * Auth: Vasco Eti
- */
-
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,14 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned int mul;
+	unsigned long mul;
 	int i, j;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	for (i =1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
