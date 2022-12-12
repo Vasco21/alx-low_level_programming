b91fcba66a75593008f5495a81ed5bbad863268a
Vasco21 <vascoeti123@gmail.com>
2022-12-12 13:44:23 -0800

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != e && c != q)
+
+			printf("%c", c);
+	}
+
+		printf("\n");
+	return (0);
+}
