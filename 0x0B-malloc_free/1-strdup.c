8147c860cb0aa1660e0da049b4d5c7d0f1748990
Vasco21 <vascoeti123@gmail.com>
2023-03-21 16:14:56 -0700

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,52 +1,25 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _strlen - return length of a string
- *
- * @s: char type
- * Return:  length of string
+ * _strdup - copies input string in newly allocated memory
+ * @str: input string
+ * Return: pointer to duplicate string or NULL if failure
  */
 char *_strdup(char *str)
 {
-	int a;
+	char *pstr;
+	unsigned int i = 0;
 
-	for (a = 0; s[a] != '\0'; a++)
-	{
-
-	}
-	return (a);
-}
-
-/**
- * *_strdup - function to return pointer to string
- * @str: pointer to string array input
- * Return: pointer to string created
- */
-
-char *_strdup(char *str)
-{
-	char *ptr;
-	int size;
-	int x;
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	size = _strlen(str) + 1;
-	ptr = malloc(size * sizeof(char));
-
-	x = 0;
-	while (x < size)
-	{
-		if (ptr == NULL)
-		{
-			return (NULL);
-	}
-		ptr[x] = str[x];
-		x++;
-	}
-	return (ptr);
+	if (str == 0)
+		return (0);
+	while (str[i] != 0)
+		i++;
+	i++;
+	pstr = malloc(sizeof(char) * i);
+	for (i = 0; str[i] != 0; i++)
+		pstr[i] = str[i];
+	pstr[i] = 0;
+	return (pstr);
 }
