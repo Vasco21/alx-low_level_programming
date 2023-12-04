5e3dcc5180323f112b0df644a56c9dba054ed19a
Vasco21 <vascoeti123@gmail.com>
2023-12-03 16:55:13 -0800

    update

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,38 @@
+// 100-operations.c
+
+#include <stdio.h>
+
+// Function to add two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Function to subtract two integers
+int sub(int a, int b) {
+    return a - b;
+}
+
+// Function to multiply two integers
+int mul(int a, int b) {
+    return a * b;
+}
+
+// Function to divide two integers
+int div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Error: Division by zero\n");
+        return 0;
+    }
+}
+
+// Function to find the remainder of the division of two integers
+int mod(int a, int b) {
+    if (b != 0) {
+        return a % b;
+    } else {
+        printf("Error: Modulo by zero\n");
+        return 0;
+    }
+}
